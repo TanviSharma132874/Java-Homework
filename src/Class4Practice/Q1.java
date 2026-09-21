405563d3bdee87147d3352c3cfbd33457c718a1c
TanviSharma132874 <tanvisharma352007@gmail.com>
2026-09-21 18:12:02 +0530

    Java DSA Practice

---
+++ src/Class4Practice/Q1.java
@@ -0,0 +1,14 @@
+package Class4Practice;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:-");
+        int num = sc.nextInt();
+        for(int i =1; i<=num; i++){
+            System.out.print(" "+i);
+        }
+    }
+}
